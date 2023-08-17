28cb738aa0f46c70247c7d2c70043611f641f610
FrancoCalcia <francocalcia02@gmail.com>
2023-08-17 20:37:31 -0300

    Funcion que convierte una lista de °C a °F

---
+++ Lista °C a °F.py
@@ -0,0 +1,10 @@
+def Conversion(c):
+    Far=[]
+    for i in c:
+        F = i * (9/5) + 32
+        Far.append(F)
+    print(Far)
+    return
+
+GradosCelsius=[43, 21, 12, 5, 36]
+Conversion(GradosCelsius)
